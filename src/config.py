2ed1258a224ac169daa95ef34fef83958356aeb8
Likith <68471226+liki-boss@users.noreply.github.com>
2024-12-03 12:58:31 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,14 +1,14 @@
 
 # Reddit Fetcher Configurations
-client_id = "u_0sYgf6aW2zXr34xrX8IA"  # Reddit app client ID
-client_secret = "Kvfe-GPkLYPKSh1ets9xpYsNjVw-yA"  # Reddit app client secret
+client_id = ""  # Reddit app client ID
+client_secret = ""  # Reddit app client secret
 user_agent = "news_fetcher"
 
 # News Fetcher Configurations
-api_key = "7c7cc70289dc4cffb8de20e3539f8bd6"    # News API Key
+api_key = ""    # News API Key
 
 # Gemini API Key
-gemini_api_key = "AIzaSyAYylQRr_YyOjrYkDyfmtoMU_oyrDtMjpQ"
+gemini_api_key = ""
 
 # Twitter Bearer Token
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAIx1xAEAAAAAszIZ38lmSQi4CBGl5y2LdBnUj5M%3DhCKnyw5uVvDBtcXS2Q4xma4eCajzrifQ7lP5mBQbT8bkbyrWqF"
+bearer_token = ""
